fbea011211e2b7ee10f335e422c5683307146c74
Ile Eftimov <ileeftimov@gmail.com>
2014-06-25 01:36:22 +0200

    LunchesController - add create, update, delete actions

--- app/controllers/api/v1/lunches_controller.rb
+++ app/controllers/api/v1/lunches_controller.rb
@@ -3,12 +3,38 @@ class Api::V1::LunchesController < ApplicationController
 
   def index
     @lunches = Lunch.pending
-    respond_with @lunches
+    render json: @lunches
   end
-  
+
+  def create
+    @lunch = Lunch.new(lunch_params)
+    if @lunch.save
+      render json: @lunch
+    else
+      render json: { "errors" => @lunch.errors }, status: 400
+    end
+  end
+
+  def update
+    @lunch = Lunch.find(params[:id])
+    if @lunch.update_attributes(lunch_params)
+      render json: @lunch
+    else
+      render json: { "errors" => @lunch.errors }, status: 400
+    end
+  end
+
+  def destroy
+    @lunch = Lunch.find(params[:id])
+    if @lunch.destroy
+      render json: { "success" => "Lunch has been deleted." }
+    else
+      render json: { "errors" => @lunch.errors }, status: 400
+    end
+  end
 
   private
   def lunch_params
-
+    params.require(:lunch).permit(:name, :description, :starts_on, :ends_on)
   end
 end
--- spec/controllers/api/v1/lunches_controller_spec.rb
+++ spec/controllers/api/v1/lunches_controller_spec.rb
@@ -10,4 +10,44 @@ RSpec.describe Api::V1::LunchesController, :type => :controller do
       expect(response).to be_success
     end
   end
+
+  describe 'POST #create' do
+    it 'creates a new lunch entry if lunch is valid' do
+      expect { 
+        post :create, lunch: { name: "Some lunch", description: "Lorem ipsum", 
+          starts_on: Date.today + 1.day, ends_on: Date.today + 2.days }, format: :json
+      }.to change(Lunch, :count).by(1)
+      expect(response).to be_success
+    end
+
+    it 'returns error if lunch is invalid' do
+      post :create, lunch: { name: 'Some lunch' } #missing other params
+      json = JSON.parse(response.body)
+      expect(json["errors"]).to have_key("description") 
+      expect(json["errors"]).to have_key("starts_on") 
+      expect(json["errors"]).to have_key("ends_on") 
+      expect(response).to_not be_success
+    end
+  end
+
+  describe 'PATCH #update' do
+    it 'updates a lunch entry' do
+      lunch = FactoryGirl.create :pending_lunch
+      # sanity check
+      expect(lunch.name).to eq "Some lunch in a restaurant"
+      patch :update, id: lunch.id, lunch: { name: "Ile's lunch" }
+      json = JSON.parse(response.body) 
+      expect(json["name"]).to eq "Ile's lunch"
+      expect(response).to be_success
+    end
+  end
+
+  describe "DELETE destroy" do
+    it 'deletes a lunch entry' do
+      lunch = FactoryGirl.create :pending_lunch
+      expect { 
+        delete :destroy, id: lunch.id
+      }.to change(Lunch, :count).by(-1)
+    end
+  end
 end
